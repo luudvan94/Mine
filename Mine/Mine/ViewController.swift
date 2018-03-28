2add057dff784c16e1b35b37602d4a7f5442414e
Van Luu <valu4298@gmail.com>
2018-03-28 11:54:15 -0500

    Initialize Project

---
+++ Mine/Mine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mine
+//
+//  Created by Van Luu on 3/28/18.
+//  Copyright © 2018 Luu Dinh Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
